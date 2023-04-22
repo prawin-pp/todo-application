1baca51d23b322e7069970079089159ebec6a0cb
Prawin Rungpiriyadej <parwin.pp@gmail.com>
2023-04-22 17:23:45 +0700

    migration: add script to create users, todos, todo_tasks tables

---
+++ api/migrations/000004_create_todo_tasks_table.up.sql
@@ -0,0 +1,19 @@
+BEGIN;
+
+CREATE TABLE IF NOT EXISTS todo_tasks (
+    id UUID PRIMARY KEY DEFAULT UUID_GENERATE_V4(),
+    name TEXT NOT NULL,
+    description TEXT,
+    completed BOOLEAN DEFAULT FALSE,
+    due_date DATE,
+    sort_order INTEGER NOT NULL,
+    todo_id UUID NOT NULL,
+    user_id UUID NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+    deleted_at TIMESTAMP WITH TIME ZONE,
+    FOREIGN KEY (todo_id) REFERENCES todos(id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+COMMIT;
