ec454e914f8fdfe30641a4e4d6e90d5b084ff6a9
Prawin Rungpiriyadej <parwin.pp@gmail.com>
2023-04-24 11:35:43 +0700

    refactor: change get user id from context

--- api/internal/todo/get.go
+++ api/internal/todo/get.go
@@ -3,13 +3,12 @@ package todo
 import (
 	"net/http"
 
-	"github.com/parwin-pp/todo-application/internal/auth"
+	"github.com/parwin-pp/todo-application/internal"
 	"github.com/uptrace/bunrouter"
 )
 
 func (s *Server) HandleGetTodos(w http.ResponseWriter, r bunrouter.Request) error {
-	userID := r.Context().Value(auth.AuthContextKey{})
-	uid, _ := userID.(string)
+	uid := internal.GetUserIDFromContext(r.Context())
 
 	s.db.GetAll(uid)
 	return nil
