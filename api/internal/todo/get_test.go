950560eb00b6c3593397aadc06601a5e418ab6f7
Prawin Rungpiriyadej <parwin.pp@gmail.com>
2023-04-24 11:02:08 +0700

    refactor: use test todos context

--- api/internal/todo/get_test.go
+++ api/internal/todo/get_test.go
@@ -1,29 +1,22 @@
 package todo
 
 import (
-	"net/http"
-	"net/http/httptest"
 	"testing"
 
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/require"
-	"github.com/uptrace/bunrouter"
 )
 
 func TestGetTodos(t *testing.T) {
 	userID := uuid.New()
-	router := bunrouter.New()
-	server := NewServer(nil)
-	router.GET("/todos", server.HandleGetTodos)
 
 	t.Run("should return http status 200 when called", func(t *testing.T) {
-		w := httptest.NewRecorder()
-		req := httptest.NewRequest(http.MethodGet, "/todos", nil)
+		testCtx := newTestGetTodosContext(t)
+		testCtx.createTodo(userID, "MOCK_TODO")
 
-		err := router.ServeHTTPError(w, req)
+		res := testCtx.sendRequest()
 
-		require.NoError(t, err)
-		require.Equal(t, 200, w.Result().StatusCode)
+		require.Equal(t, 200, res.Result().StatusCode)
 	})
 
 	t.Run("should call get todos from database when called", func(t *testing.T) {
