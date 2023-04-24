1ef7c1f7f349dfd1a3c546087a355555c8498e97
Prawin Rungpiriyadej <parwin.pp@gmail.com>
2023-04-24 15:11:22 +0700

    refactor: move mock file

--- api/internal/todo/get_test.go
+++ api/internal/todo/get_test.go
@@ -3,12 +3,79 @@ package todo
 import (
 	"encoding/json"
 	"errors"
+	"net/http"
+	"net/http/httptest"
 	"testing"
+	"time"
 
 	"github.com/google/uuid"
 	"github.com/parwin-pp/todo-application/internal/model"
 	"github.com/stretchr/testify/require"
+	"github.com/uptrace/bunrouter"
 )
+
+type mockGetTodosDatabase struct {
+	Database
+
+	NumberOfCalled int
+	CallWithParams []string
+	ReturnTodos    []model.Todo
+	ReturnError    error
+}
+
+func (m *mockGetTodosDatabase) GetAll(userID string) ([]model.Todo, error) {
+	m.NumberOfCalled++
+	m.CallWithParams = append(m.CallWithParams, userID)
+	return m.ReturnTodos, m.ReturnError
+}
+
+type testGetTodosContext struct {
+	t          *testing.T
+	router     *bunrouter.Router
+	db         *mockGetTodosDatabase
+	withUserID string
+}
+
+func (testContext *testGetTodosContext) createTodo(userID uuid.UUID, name string) *model.Todo {
+	todo := model.Todo{
+		ID:        uuid.New(),
+		Name:      name,
+		UserID:    userID,
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	}
+	testContext.db.ReturnTodos = append(testContext.db.ReturnTodos, todo)
+	return &todo
+}
+
+func (testContext *testGetTodosContext) withGetTodosError(err error) {
+	testContext.db.ReturnError = err
+}
+
+func (testContext *testGetTodosContext) requestWithUserID(userID uuid.UUID) *httptest.ResponseRecorder {
+	testContext.withUserID = userID.String()
+
+	w := httptest.NewRecorder()
+	req := httptest.NewRequest(http.MethodGet, "/todos", nil)
+
+	err := testContext.router.ServeHTTPError(w, req)
+	require.NoError(testContext.t, err)
+
+	return w
+}
+
+func newTestGetTodosContext(t *testing.T) *testGetTodosContext {
+	testCtx := &testGetTodosContext{t: t}
+	testCtx.router = bunrouter.New(bunrouter.Use(mockAuthMiddleware(func() string {
+		return testCtx.withUserID
+	})))
+	testCtx.db = &mockGetTodosDatabase{}
+
+	server := NewServer(testCtx.db)
+	testCtx.router.GET("/todos", server.HandleGetTodos)
+
+	return testCtx
+}
 
 func TestGetTodos(t *testing.T) {
 	userID := uuid.New()
--- api/internal/todo/server_test.go
+++ api/internal/todo/server_test.go
@@ -3,77 +3,10 @@ package todo
 import (
 	"context"
 	"net/http"
-	"net/http/httptest"
-	"testing"
-	"time"
 
-	"github.com/google/uuid"
 	"github.com/parwin-pp/todo-application/internal/auth"
-	"github.com/parwin-pp/todo-application/internal/model"
-	"github.com/stretchr/testify/require"
 	"github.com/uptrace/bunrouter"
 )
-
-type testGetTodosContext struct {
-	t          *testing.T
-	router     *bunrouter.Router
-	db         *mockTodoDatabase
-	withUserID string
-}
-
-func (testContext *testGetTodosContext) createTodo(userID uuid.UUID, name string) *model.Todo {
-	todo := model.Todo{
-		ID:        uuid.New(),
-		Name:      name,
-		UserID:    userID,
-		CreatedAt: time.Now(),
-		UpdatedAt: time.Now(),
-	}
-	testContext.db.ExistsTodos = append(testContext.db.ExistsTodos, todo)
-	return &todo
-}
-
-func (testContext *testGetTodosContext) withGetTodosError(err error) {
-	testContext.db.ReturnError = err
-}
-
-func (testContext *testGetTodosContext) requestWithUserID(userID uuid.UUID) *httptest.ResponseRecorder {
-	testContext.withUserID = userID.String()
-
-	w := httptest.NewRecorder()
-	req := httptest.NewRequest(http.MethodGet, "/todos", nil)
-
-	err := testContext.router.ServeHTTPError(w, req)
-	require.NoError(testContext.t, err)
-
-	return w
-}
-
-func newTestGetTodosContext(t *testing.T) *testGetTodosContext {
-	testCtx := &testGetTodosContext{t: t}
-	testCtx.router = bunrouter.New(bunrouter.Use(mockAuthMiddleware(func() string {
-		return testCtx.withUserID
-	})))
-	testCtx.db = &mockTodoDatabase{}
-
-	server := NewServer(testCtx.db)
-	testCtx.router.GET("/todos", server.HandleGetTodos)
-
-	return testCtx
-}
-
-type mockTodoDatabase struct {
-	NumberOfCalled int
-	CallWithParams []string
-	ExistsTodos    []model.Todo
-	ReturnError    error
-}
-
-func (m *mockTodoDatabase) GetAll(userID string) ([]model.Todo, error) {
-	m.NumberOfCalled++
-	m.CallWithParams = append(m.CallWithParams, userID)
-	return m.ExistsTodos, m.ReturnError
-}
 
 func mockAuthMiddleware(getUserID func() string) bunrouter.MiddlewareFunc {
 	return func(next bunrouter.HandlerFunc) bunrouter.HandlerFunc {
