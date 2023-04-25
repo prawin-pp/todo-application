9587bd2ca2ad581807217620c15fc72bbb569afa
Prawin Rungpiriyadej <parwin.pp@gmail.com>
2023-04-25 15:02:25 +0700

    fix: due_date must set nullzero to allow empty string

--- api/internal/model/todo_task.go
+++ api/internal/model/todo_task.go
@@ -14,13 +14,13 @@ type TodoTask struct {
 	Name        string       `json:"name" bun:"name,type:text"`
 	Description string       `json:"description" bun:"description,type:text"`
 	Completed   bool         `json:"completed" bun:"completed,type:boolean,default:false"`
-	DueDate     string       `json:"dueDate" bun:"due_date,type:date"`
+	DueDate     string       `json:"dueDate" bun:"due_date,type:date,nullzero"`
 	SortOrder   int64        `json:"sortOrder" bun:"sort_order,type:integer,notnull"`
 	TodoID      uuid.UUID    `json:"-" bun:"todo_id,type:uuid,notnull"`
 	UserID      uuid.UUID    `json:"-" bun:"user_id,type:uuid,notnull"`
 	CreatedAt   time.Time    `json:"createdAt" bun:"created_at,type:timestamptz,default:current_timestamp"`
 	UpdatedAt   time.Time    `json:"updatedAt" bun:"updated_at,type:timestamptz,default:current_timestamp"`
-	DeletedAt   bun.NullTime `json:"-" bun:"deleted_at,type:timestamptz,soft_delete"`
+	DeletedAt   bun.NullTime `json:"-" bun:"deleted_at,type:timestamptz,soft_delete,nullzero"`
 }
 
 type CreateTodoTaskRequest struct {
