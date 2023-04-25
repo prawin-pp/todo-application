553254285589275cd21a0741f5266981a5c7f993
Prawin Rungpiriyadej <parwin.pp@gmail.com>
2023-04-26 00:09:10 +0700

    fix: change file's extension to commonjs

---
+++ web/tailwind.config.cjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          ...require("daisyui/src/colors/themes")["[data-theme=light]"],
+          primary: '#ec008c'
+        }
+      }
+    ]
+  }
+};
