ee2844d48561b342326037e896f9ae61bc9a17d3
Prawin Rungpiriyadej <parwin.pp@gmail.com>
2023-04-25 20:37:59 +0700

    feat: implement get sql connection

--- api/internal/postgres/postgres.go
+++ api/internal/postgres/postgres.go
@@ -1,7 +1,13 @@
 package postgres
 
 import (
+	"database/sql"
+	"fmt"
+
+	"github.com/parwin-pp/todo-application/internal/config"
 	"github.com/uptrace/bun"
+	"github.com/uptrace/bun/dialect/pgdialect"
+	"github.com/uptrace/bun/driver/pgdriver"
 )
 
 type DB struct {
@@ -10,4 +16,20 @@ type DB struct {
 
 func NewDB(db *bun.DB) *DB {
 	return &DB{db: db}
+}
+
+func DSN(config config.DatabaseConfig) string {
+	return fmt.Sprintf("postgres://%s:%s@%s:%d/%s?sslmode=disable",
+		config.User,
+		config.Password,
+		config.Host,
+		config.Port,
+		config.Name,
+	)
+}
+
+func GetConnection(config config.DatabaseConfig) *bun.DB {
+	dsn := DSN(config)
+	sqldb := sql.OpenDB(pgdriver.NewConnector(pgdriver.WithDSN(dsn)))
+	return bun.NewDB(sqldb, pgdialect.New())
 }
