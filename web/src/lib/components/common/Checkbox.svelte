ecbfa2b5a79402c347ddd835a4e0a0be9ef7a99a
Prawin Rungpiriyadej <parwin.pp@gmail.com>
2023-04-29 23:52:15 +0700

    feat: implement Checkbox component

---
+++ web/src/lib/components/common/Checkbox.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  export let checked: boolean = false;
+</script>
+
+<div class="flex">
+  <input
+    bind:checked
+    type="checkbox"
+    value=""
+    class="h-5 w-5 rounded-full border-gray-300 bg-white text-primary focus:ring-2 focus:ring-primary"
+    on:change
+    on:click|stopPropagation
+  />
+</div>
