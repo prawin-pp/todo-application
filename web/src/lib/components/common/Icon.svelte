80c11cdcefa66c2dc1f67653f1aeed7caea76bf9
Prawin Rungpiriyadej <parwin.pp@gmail.com>
2023-04-29 23:52:40 +0700

    feat: implement material Icon component

---
+++ web/src/lib/components/common/Icon.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  export let shape: 'filled' | 'outlined' | 'rounded' = 'filled';
+
+  let className: string = '';
+  export { className as class };
+
+  const styles = {
+    filled: 'material-icons',
+    outlined: 'material-icons-outlined',
+    rounded: 'material-icons-round',
+  };
+
+  $: iconClass = styles[shape] || '';
+</script>
+
+<span class="{className} {iconClass} select-none" on:click>
+  <slot />
+</span>
