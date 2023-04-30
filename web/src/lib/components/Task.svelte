4249eefe04475037ec8dab8f1382cf864981b132
Prawin Rungpiriyadej <parwin.pp@gmail.com>
2023-04-30 16:15:51 +0700

    feat: implement Task component

--- web/src/lib/components/Task.svelte
+++ web/src/lib/components/Task.svelte
@@ -1,14 +1,49 @@
 <script lang="ts">
-  export let name: string | undefined | null = undefined;
-  export let description: string | undefined | null = undefined;
-  export let dueDate: string | undefined | null = undefined;
+  import dayjs from 'dayjs';
+  import { createEventDispatcher } from 'svelte';
+  import type { Task } from '../../models/task.model';
+  import Checkbox from './common/Checkbox.svelte';
+  import Icon from './common/Icon.svelte';
+
+  let className = '';
+  export let task: Task;
+  export { className as class };
+
+  $: isExpired = task.dueDate && dayjs(task.dueDate).isBefore(dayjs(), 'day');
+  $: isNearExpired = task.dueDate && dayjs(task.dueDate).diff(dayjs(), 'day') <= 7 && !isExpired;
+
+  const dispatch = createEventDispatcher();
+
+  const handleCheckboxChange = (e: Event) => {
+    e.stopPropagation();
+    const target = e.target as HTMLInputElement;
+    dispatch('checked', target.checked);
+  };
 </script>
 
-<div class="flex min-h-[40px] gap-x-5 bg-white p-5">
-  <div>
-    <input type="checkbox" class="checkbox-primary checkbox" />
-  </div>
+<div class="{className || ''} flex min-h-[40px] gap-x-5 p-5" on:click>
+  <Checkbox checked={task.completed} on:change={handleCheckboxChange} />
   <div class="flex flex-col">
-    <span class="font-bold text-black">{name}</span>
+    <span class="font-bold text-gray-700" class:line-through={task.completed}>
+      {task.name || ''}
+    </span>
+    <div
+      class="flex max-h-[500px] flex-col overflow-hidden transition-all"
+      class:!max-h-0={task.completed}
+    >
+      {#if task.description}
+        <span class="text-gray-500">{task.description || ''}</span>
+      {/if}
+      {#if task.dueDate}
+        <div
+          class="mt-1 flex items-center gap-x-2.5 text-green-500"
+          class:!text-red-500={isExpired}
+          class:!text-amber-500={isNearExpired}
+        >
+          <Icon shape="rounded" class="text-[20px]">date_range</Icon>
+          <span>{dayjs(task.dueDate).format('DD MMM YYYY')}</span>
+        </div>
+      {/if}
+    </div>
   </div>
 </div>
--- web/tests/components/task.test.ts
+++ web/tests/components/task.test.ts
@@ -1,21 +1,87 @@
-import { render, screen } from '@testing-library/svelte';
-import { describe, expect, it } from 'vitest';
+import { fireEvent, render, screen } from '@testing-library/svelte';
+import { describe, expect, it, vi } from 'vitest';
 import Task from '../../src/lib/components/Task.svelte';
 
 describe('Task component', () => {
   it('should show the task name given name = "Test task"', () => {
-    render(Task, { name: 'Test task' });
+    const task: Task = { name: 'Test task', description: '', dueDate: '', completed: false };
+    render(Task, { task });
 
     const taskName = screen.getByText('Test task');
 
-    expect(taskName.innerHTML).toBe('Test task');
+    expect(taskName?.innerHTML).toBe('Test task');
   });
 
   it('should show the task name given name = "Another task"', () => {
-    render(Task, { name: 'Another task' });
+    const task: Task = { name: 'Another task', description: '', dueDate: '', completed: false };
+    render(Task, { task });
 
     const taskName = screen.getByText('Another task');
 
-    expect(taskName.innerHTML).toBe('Another task');
+    expect(taskName?.innerHTML).toBe('Another task');
+  });
+
+  it('should not show the task name given not set name', () => {
+    const task: Task = { name: '', description: '', dueDate: '', completed: false };
+    render(Task, { task });
+
+    const taskName = screen.queryByText('undefined');
+
+    expect(taskName).toBeNull();
+  });
+
+  it('should show the task description given description = "Test description"', () => {
+    const task: Task = { name: '', description: 'Test description', dueDate: '', completed: false };
+    render(Task, { task });
+
+    const taskDescription = screen.getByText('Test description');
+
+    expect(taskDescription.innerHTML).toBe('Test description');
+  });
+
+  it('should not show the task description when not set description', () => {
+    const task: Task = { name: '', description: '', dueDate: '', completed: false };
+    render(Task, { task });
+
+    const taskDescription = screen.queryByText('undefined');
+
+    expect(taskDescription).toBeNull();
+  });
+
+  it('should show the task due date in format "DD MMM YYYY" given dueDate = "2021-10-10"', () => {
+    const task: Task = { name: '', description: '', dueDate: '2021-10-10', completed: false };
+    render(Task, { task });
+
+    const taskDueDate = screen.queryByText('10 Oct 2021');
+
+    expect(taskDueDate).not.toBeNull();
+  });
+
+  it('should dispatch checked event with value=false when checkbox is checked', async () => {
+    let completed = true;
+    const handleChecked = vi.fn().mockImplementation((e: CustomEvent) => (completed = e.detail));
+    const task: Task = { name: '', description: '', dueDate: '2021-10-10', completed: true };
+
+    const { component } = render(Task, { task });
+    const checkbox = screen.getByRole('checkbox');
+    component.$on('checked', handleChecked);
+
+    await fireEvent.click(checkbox);
+
+    expect(completed).toEqual(false);
+  });
+
+  it('should dispatch checked event with value=true when checkbox is unchecked', async () => {
+    let completed = false;
+    const handleChecked = vi.fn().mockImplementation((e: CustomEvent) => (completed = e.detail));
+    const task: Task = { name: '', description: '', dueDate: '2021-10-10', completed: false };
+
+    const { component } = render(Task, { task });
+    const checkbox = screen.getByRole('checkbox');
+    component.$on('checked', handleChecked);
+
+    await fireEvent.click(checkbox);
+
+    expect(completed).toEqual(true);
   });
 });
